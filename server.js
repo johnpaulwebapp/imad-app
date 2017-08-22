3a9341024179a9fe27afbe9db3a487c2fb3c9041
johnpaulwebapp <31134871+johnpaulwebapp@users.noreply.github.com>
2017-08-22 19:02:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,12 +22,50 @@ var content = {
                 </p>`
    
     };
+    function createtemplate (data) {
+       var title = data.title;
+       var date = data.date;
+       var heading = data.heading;
+       var content = data.content;
+       return htmltemplate;
+    }
+    
+    var htmltemplate=`
+    
+        <html>
+    <head>
+        <Title> 
+            ${Title}
+        </Title>
+          <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class='container'>
+            <div>
+            <a href ='/'>home</a>
+            </div>
+        <hr/>
+                <div>
+            <h3>
+               ${heading}
+            </h3>
+                    <div>
+                ${date}
+            </div>
+                        <div>
+               ${content}
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+`;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.sendFile(createtemplate (article-one.html));
 });
 app.get('/article-two', function (req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
