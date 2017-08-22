ccb9d0a69ea88c19addd93207cbcc7028305e2f9
johnpaulwebapp <31134871+johnpaulwebapp@users.noreply.github.com>
2017-08-22 19:10:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req,res) {
-    res.sendFile(createtemplate (article-one.html));
+    res.send(createtemplate (article-one.html));
 });
 app.get('/article-two', function (req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
