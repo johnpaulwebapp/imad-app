f0cd60f403ebc3a1d719736bb658e3f653ac8b19
johnpaulwebapp <31134871+johnpaulwebapp@users.noreply.github.com>
2017-08-25 21:04:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,12 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter = 0;
+app.get('/counter', funtion (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+    });
 app.get('/:articleName', function (req,res) {
     //articleName = article-one
     //articles[articleName] == {} content object for article one
