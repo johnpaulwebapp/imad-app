e097215416c111cc9ec7dd383704d1c919ebce7c
johnpaulwebapp <31134871+johnpaulwebapp@users.noreply.github.com>
2017-09-01 20:42:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ var element=document.getelementById('main-text'
 );
 
 element.innerHTML='New Value';
+
+//Move the Images
+var img =document.getelementById ('img');
+img.onclick=function (){
+  img.style.marginLeft = '100px'  ;
+};
